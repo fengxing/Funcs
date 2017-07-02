33aeea1dcd0cdb52e289e8e8bc665927501fb34f
陈卓杰 <117822597@163.com>
2017-07-03 00:39:58 +0800

    how create a webapi application and add webapi dlls

---
+++ HowToBuilderWebApi/BuildWebApi/BuildWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+
+namespace BuildWebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        [Route("Values/Get")]
+        public string Get()
+        {
+            return DateTime.Now.Ticks.ToString();
+        }
+    }
+}
