80e3b71c11c29b1e0192a62ef96f7c97726db2d7
陈卓杰 <117822597@163.com>
2017-11-06 21:31:33 +0800

    添加Mutex

---
+++ Mutex/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MutexDemo
+{
+    class Program
+    {
+        public static Mutex m = new Mutex(true, "Text");
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Contains("a"))
+                {
+                    while (true)
+                    {
+                        Log("a");
+                    }
+                }
+                else
+                {
+                    while (true)
+                    {
+                        Log("b");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(@"C:\MutexTestError.txt", true);
+                sw.WriteLine(ex.Message);
+                sw.Flush();
+                sw.Close();
+            }
+        }
+
+
+        public static void Log(string p)
+        {
+            m.WaitOne();
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(@"C:\MutexTest.txt", true);
+            sw.WriteLine(p + DateTime.Now.ToString());
+            sw.Flush();
+            sw.Close();
+            m.ReleaseMutex();
+        }
+    }
+}
